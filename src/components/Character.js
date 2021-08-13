236472b57fedb0b17763f3ab55bfe6fffee4904b
Brent Besselievre <brent@Brents-MacBook-Pro.local>
2021-08-13 14:17:58 -0600

    Added character details

--- src/components/Character.js
+++ src/components/Character.js
@@ -3,10 +3,31 @@ import React from 'react';
 
 const Character = (props) => {
     const { character } = props
+
+   const cmtoFeet = (cm) => {
+       const inches = cm/2.54;
+       const feet = Math.floor(inches/12);
+       const remainingInches = (inches/12 - feet) *12;
+       return `${feet}'${Math.round(remainingInches)}`
+   }
+
+   const kilosToPounds = (kilos) => {
+       return `${Math.round(kilos * 2.205)} lbs.`
+   }
+
+   
     return (
         <div>
-            <p>This is the Character Component</p>
-            <p>{character.name}</p>
+            <h3>{character.name}</h3>
+            <button>More Information</button>
+            <div className='character-data'>
+                <p>Birth Year: {character.birth_year}</p>
+                <p>Gender: {character.gender}</p>
+                <p>Eye Color: {character.eye_color}</p>
+                <p>Hair Color: {character.hair_color}</p>
+                <p>Height: {cmtoFeet(character.height)} </p>
+                <p>Weight: {kilosToPounds(character.mass)}</p>
+            </div>
         </div>
     )
 }
