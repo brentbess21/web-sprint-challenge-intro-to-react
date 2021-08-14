d0b0fe29b29bb1a43fe3edd284ed4aef20b2e613
Brent Besselievre <brent@Brents-MacBook-Pro.local>
2021-08-13 18:23:31 -0600

    Some styling fixes

--- src/components/Character.js
+++ src/components/Character.js
@@ -7,7 +7,7 @@ import { Collapse } from 'reactstrap';
 const StyledCard = styled.div `
     display: flex;
     flex-direction: column;
-    justify-content: space-evenly;
+    justify-content: center;
     align-items: center;
     border: 2px solid black;
     width: 50%;
@@ -20,9 +20,15 @@ const StyledCard = styled.div `
         font-size: 2.5rem;
     }
 
-    button{
-        width: 220px;
+    span {
         font-family: 'Titillium Web', sans-serif;
+        text-transform: uppercase;
+    }
+
+    button{
+        width: 250px;
+        font-family: 'Titillium Web', sans-serif;
+        margin-top: 20px;
         padding: 10px 20px;
         border:3px solid #ffe81f;
         box-sizing: border-box;
@@ -38,6 +44,11 @@ const StyledCard = styled.div `
          border-color:white;
         background-color: black;
         }
+
+    .character-data {
+        display: flex;
+        justify-content: space-between;
+    }
 
 `
 
@@ -66,12 +77,16 @@ const toggle = () => setIsOpen(!isOpen);
             <button  onClick={toggle} >More Info</button>
             <Collapse isOpen={isOpen}>
             <div className='character-data'>
-                <p>Birth Year: {character.birth_year}</p>
-                <p>Gender: {character.gender}</p>
-                <p>Eye Color: {character.eye_color}</p>
-                <p>Hair Color: {character.hair_color}</p>
-                <p>Height: {cmtoFeet(character.height)} </p>
-                <p>Weight: {kilosToPounds(character.mass)}</p>
+                <div className='data-container'>
+                    <p><span>Birth Year:</span> {character.birth_year}</p>
+                    <p><span>Gender:</span> {character.gender}</p>
+                    <p><span>Eye Color:</span> {character.eye_color}</p>
+                </div>
+                <div className='data-container'>
+                    <p><span>Hair Color:</span> {character.hair_color}</p>
+                    <p><span>Height:</span> {cmtoFeet(character.height)} </p>
+                    <p><span>Weight:</span> {kilosToPounds(character.mass)}</p>
+                </div>
             </div>
             </Collapse>
         </StyledCard>
